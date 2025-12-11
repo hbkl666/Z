8464ef88b3440a72df6fbb27a4c64af45d74c1e2
赤雩 <2806443982@qq.com>
2025-12-11 17:20:18 +0800

    delete

--- car.h
+++ car.h
@@ -1,7 +1,25 @@
 #include<stdio.h>
 void add ();
-void delete(int address);
+void delete(int address)
 double  get_money(int time);
 void search(int addresss);
 void desplay_all();
+
+void delete(int address)
+{
+        int i,j;
+for(i=0;i<car_count;i++)
+{
+  if(vehicle[i].address==address)
+        {
+	for(j=i;j<car_count-1;j++)
+		{
+		vehicle[j]=vehicle[j+1];
+		}
+		car_count--;
+		printf("第%d辆车已出库",address);
+        }
+}
+
+
 
